292fecaacd1aaa574aca914bc05affbd556c044b
Git Gud <irina.kozhevnikov@gmail.com>
2023-03-26 23:45:59 -0400

    added more js code, fiddled with css

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
 const startButton=document.getElementById("start-button");
 const welcomeMessage=document.getElementById("welcome");
-const mainDiv=document.getElementById("inner-html");
 
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+
+//array of tasks
+const tasks=[];
+
+const totalTasks=document.createElement("h3");
+const totalTasksMessage=" total tasks";
+
+const taskList=document.createElement("ul");
+const form=document.getElementById("form")
+
+const taskInput=document.createElement("input");
+const addButton=document.createElement("span");
+
+taskList.setAttribute("id","task-list");
+
+taskInput.setAttribute("type","text");
+taskInput.setAttribute("placeholder","Write task here...");
+taskInput.setAttribute("id","task-input");
+
+addButton.setAttribute("onclick","addTask()");
+addButton.setAttribute("id","add-task");
+addButton.innerHTML="Add";
 
 function openTaskList()
 {
-    let taskInput=document.createElement("input");
-    let addButton=document.createElement("span");
-
-    taskInput.setAttribute("type","text");
-    taskInput.setAttribute("placeholder","Write task here...");
-    taskInput.setAttribute("id","task-input");
-
-    addButton.setAttribute("onclick","addTask()");
-    addButton.setAttribute("id","add-task");
-    addButton.innerHTML="Add";
-
     welcomeMessage.replaceWith(taskInput);
     startButton.replaceWith(addButton);
+    form.appendChild(taskList);
 }
-function addTask()
+
+function addTask(e)
 {
     let newTask=document.createElement("li");
+    newTask.setAttribute("class","list");
     let taskName=document.getElementById("task-input").value;
     let checkInput=document.createTextNode(taskName);
 
     newTask.appendChild(checkInput);
+
+    e.preventDefault();
 
     if(taskName==='')
     {
@@ -39,11 +48,31 @@ function addTask()
     }
     else
     {
+        tasks.push(taskName);
         document.getElementById("task-list").appendChild(newTask);
+        totalTasks.innerHTML=tasks.length;
+        totalTasks.append(totalTasksMessage);
+        form.appendChild(totalTasks);
+        //appendCloseButton(newTask,tasks[]);
     }
-
+    
     document.getElementById("task-input").value="";
 }
 
+function appendCloseButton(task,index)
+{
+    let span = document.createElement("SPAN");
+    let x = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.setAttribute("onclick","removeTask");
+    span.appendChild(x);
+    task.appendChild(span);
+}
+
+function removeTask()
+{
+
+}
+
 startButton.addEventListener("click",openTaskList);
-addButton.addEventListener("click",addTask)
+addButton.addEventListener("click",addTask);
